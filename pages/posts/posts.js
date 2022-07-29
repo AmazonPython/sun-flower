66f88812e5b644f8e4883dd96785251dd4078fba
AmazonPython <amazon3@foxmail.com>
2022-07-29 15:09:56 +0800

    Add assets. 🍱

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -13,12 +13,7 @@ Page({
      */
     onLoad: function (options) {
         // 创建 + 更新
-        let content = {
-            headImgSrc: {
-                one: "/images/post/genshin.jpg",
-                two: "/images/post/curiosity.jpg",
-                three: "/images/post/sls.jpg"
-            },
+        let content = [{
             postDate: "2002-02-02",
             avatar: "/images/avatar/4.png",
             author: "Hugo",
@@ -28,9 +23,22 @@ Page({
             view: "666",
             collection: "33",
             postId: "3"
-        };
-    
-        this.setData(content);
+        },{
+            postDate: "2002-02-02",
+            avatar: "/images/avatar/3.png",
+            author: "Dante",
+            title: "石壕吏",
+            content: "暮投石壕村，有吏夜捉人。\n老翁逾墙走，老妇出门看。\n吏呼一何怒！妇啼一何苦！\n听妇前致词：三男邺城戍。\n一男附书至，二男新战死。\n存者且偷生，死者长已矣！\n室中更无人，惟有乳下孙。\n有孙母未去，出入无完裙。\n老妪力虽衰，请从吏夜归。\n急应河阳役，犹得备晨炊。\n夜久语声绝，如闻泣幽咽。\n天明登前途，独与老翁别。\n\n759年（唐肃宗乾元二年）春，四十八岁的杜甫由左拾遗贬为华州司功参军。他离开洛阳，历经新安、石壕、潼关，赶往华州任所。这时，杜甫正由新安县继续西行，投宿石壕村，遇到吏卒深夜捉人，于是实录所见所闻，写成这篇不朽的诗作。",
+            postImgSrc: "/images/post/angry.jpg",
+            view: "444",
+            collection: "22",
+            postId: "4"
+        }];
+
+        // 将数组包装成对象
+        this.setData({
+            posts:content
+        });
     },
 
     /**
