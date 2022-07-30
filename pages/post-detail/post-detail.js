46c42fa4ead318fb1417b81132e81af208295f48
AmazonPython <amazon3@foxmail.com>
2022-07-30 23:04:14 +0800

    Build article details page. 🎨💄💥

--- pages/post-detail/post-detail.js
+++ pages/post-detail/post-detail.js
@@ -1,88 +1,66 @@
-// pages/posts/posts.js
-// 导入数据文件
-import {postList} from '../../data/data.js';
-
+// pages/post-detail/post-detail.js
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        a: "箜篌引"
+
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-        // 创建 + 更新
-        this.setData({
-            postList
-        });
-    },
 
-    /**
-     * 帖子详情页
-     */
-    onGoToDetail() {
-        wx.navigateTo({
-          url: '/pages/post-detail/post-detail',
-        })
-    },
-
-    /**
-     * 阻止事件冒泡
-     */
-    onMaxImage(event) {
-        
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {
+    onReady() {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
+    onShow() {
 
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function () {
+    onHide() {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function () {
+    onUnload() {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
+    onPullDownRefresh() {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {
+    onReachBottom() {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
+    onShareAppMessage() {
 
     }
 })
