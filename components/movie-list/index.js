e3964aa0293e82db90fec9ea7592d1c594711a40
AmazonPython <amazon3@foxmail.com>
2022-08-22 22:19:11 +0800

    Using external classes in movie components. 🎨 💄 ✅

--- components/movie-list/index.js
+++ components/movie-list/index.js
@@ -2,9 +2,12 @@
 Component({
     /**
      * 组件的属性列表
+     * 外部样式类：组件样式由调用页面进行控制，只需在组件标签上加上外部样式关键字并在 CSS 页面编写样式即可
+     * externalClasses: ['your-class'] 必须是数组
      */
+    externalClasses:['movie-class'],
     properties: {
-
+        
     },
 
     /**
