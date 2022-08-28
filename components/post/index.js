db49745ea22b35088973641f46f577937fece54b
AmazonPython <amazon3@foxmail.com>
2022-08-28 23:14:40 +0800

    Update post component. 🎨 ✅ ♻️

--- components/post/index.js
+++ components/post/index.js
@@ -16,8 +16,15 @@ Component({
 
     /**
      * 组件的方法列表
+     * 自定义组件的自定义事件产生
+     * 点击组件后的事件由组件的使用者决定
      */
     methods: {
-
+        onTap(event){
+            const pid = this.properties.res.postId
+            this.triggerEvent('posttap', {
+                pid
+            })
+        },
     }
 })
