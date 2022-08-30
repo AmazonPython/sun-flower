3525b937cafb6156570bf71d4d4ca45a0e534e0e
AmazonPython <amazon3@foxmail.com>
2022-08-30 21:40:44 +0800

    Updated movie details page. 💬 🔧 💄 🎨

--- components/movie/index.js
+++ components/movie/index.js
@@ -18,6 +18,12 @@ Component({
      * 组件的方法列表
      */
     methods: {
-
+        onGoToDetail(event){
+            // 在 properties 中可以直接获得参数，不需要在自定义组件中进行数据绑定 
+            const mid = this.properties.movie.id;
+            wx.navigateTo({
+                url: '/pages/movie-detail/movie-detail?mid=' + mid
+            })
+        }
     }
 })
